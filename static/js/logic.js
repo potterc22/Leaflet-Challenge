a5b716426c02b7274cb8fd1e25eea25b32324340
Cory Potter <pottercory22@gmail.com>
2020-11-22 21:24:20 -0600

    added basic markers for each earthquake

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("map", {
-    center: [40, -105.71],
-    zoom: 5
+    center: [28,2],
+    zoom: 2
 });
   
 // Adding tile layer to the map
@@ -14,3 +14,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
+// chose to do M4.5+ Earthquakes from the past 7 days
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+
+// Grab the data with d3
+d3.json(queryURL, function(response) {
+    console.log(response)
+    console.log(response.features[0].geometry.coordinates)
+
+      // Loop through data
+      for (var i = 0; i < response.features.length; i++) {
+        
+        // Set the data location property to a variable
+        var location = response.features[i].geometry;
+        // Check for location property
+        if (location) {
+          
+          // Add a new marker to the cluster group and bind a pop-up
+          L.marker([location.coordinates[1],location.coordinates[0]])
+            .bindPopup("<h2>" + response.features[i].properties.place + 
+                "</h2> <h3> Magnitude: " + 
+                response.features[i].properties.mag + 
+                "</h3>").addTo(myMap);
+        }
+      }
+});
