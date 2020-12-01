7353ad6a0100a3d4524d8b1da33a712957b3a177
Cory Potter <pottercory22@gmail.com>
2020-12-01 11:35:23 -0600

    added Tectonic plates layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,11 +15,72 @@ function markerSize(magnitude) {
       return magnitude * 25000;
 }
 
+
+// Adding light layer variable
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+});
+
+// Adding satellite layer variable
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+});
+
+// Adding outdoors layer variable
+var outdoorMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+});    
+
+
+// Create a baseMaps object to contain the streetmap and darkmap
+var baseMaps = {
+    Grayscale: lightMap,
+    Satellite: satelliteMap,
+    Outdoors: outdoorMap
+}
+
+// declare variable for overlay group
+var layers = {
+    Earthquakes: new L.layerGroup(),
+    TectonicPlates: new L.LayerGroup()
+};
+
+var overlayMaps = {
+    Earthquakes: layers.Earthquakes,
+    TectonicPlates: layers.TectonicPlates
+}
+
+// Creating map object
+var myMap = L.map("map", {
+    center: [28,2],
+    zoom: 3,
+    layers: [lightMap, layers.Earthquakes, layers.TectonicPlates]
+});
+
+// Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Create Earthquakes layer
+
 // Grab the data with d3
 d3.json(queryURL, function(response) {
-
-    // create variable to hold latitude and longitude values
-    var earthquakes = []
 
     // Loop through data
     for (var i = 0; i < response.features.length; i++) {
@@ -27,91 +88,59 @@ d3.json(queryURL, function(response) {
         // Set the data location property to a variable
         var location = response.features[i].geometry;
 
-        earthquakes.push(
-            L.circle([location.coordinates[1], location.coordinates[0]], {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: "black",
-                fillColor: getColor(location.coordinates[2]),
-                radius: markerSize(response.features[i].properties.mag)
-            }).bindPopup("<h2>" + response.features[i].properties.place + 
-            "</h2> <h3> Magnitude: " + response.features[i].properties.mag + "</h3>")
-        )
-        console.log(markerSize(response.features[i].properties.mag))
+        L.circle([location.coordinates[1], location.coordinates[0]], {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "black",
+            fillColor: getColor(location.coordinates[2]),
+            radius: markerSize(response.features[i].properties.mag)
+        }).bindPopup("<h2>" + response.features[i].properties.place + 
+        "</h2> <h3> Magnitude: " + response.features[i].properties.mag + "</h3>").addTo(layers.Earthquakes)
     }
     
-    console.log(earthquakes)
-    // Adding light layer variable
-    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/light-v10",
-        accessToken: API_KEY
-    });
+    
+})
 
-    // Adding satellite layer variable
-    var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/satellite-v9",
-        accessToken: API_KEY
-    });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Adding outdoors layer variable
-    var outdoorMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/outdoors-v11",
-        accessToken: API_KEY
-    });    
+// Create tectonic plates layer
 
-    // Create a separate layer group for earthquakes below. 
-    var earthquakeLayer = L.layerGroup(earthquakes)
-    console.log(earthquakeLayer)
-    // Create a baseMaps object to contain the streetmap and darkmap
-    var baseMaps = {
-        Grayscale: lightMap,
-        Satellite: satelliteMap,
-        Outdoors: outdoorMap
+platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
+
+// Grab the tectonic plates data with d3
+d3.json(platesURL, function(response) {
+
+    // Loop through data
+    for (var i = 0; i < response.features.length; i++) {
+        // create line variable with all the coordinates
+        var line = response.features[i].geometry.coordinates
+
+        L.polyline(line, {
+            color: "orange"
+        }).bindPopup("<h3> Tectonic Plate Name: " + response.features[i].properties.PlateName + "</h3>").addTo(layers.TectonicPlates)   
     }
+})
 
-    var overlayMaps = {
-        EarthquakeLayer: earthquakeLayer
-    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Creating map object
-    var myMap = L.map("map", {
-        center: [28,2],
-        zoom: 3,
-        layers: [lightMap, earthquakeLayer]
-    });
+// create the legend https://leafletjs.com/examples/choropleth/
+// https://github.com/timwis/leaflet-choropleth/blob/gh-pages/examples/legend/demo.js
 
-    // Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
-    L.control.layers(baseMaps, overlayMaps).addTo(myMap)
+var legend = L.control({position: 'bottomright'});
 
-    // create the legend
-    var legend = L.control({position: 'bottomright'});
-
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend'),
-            limits = [-10, 10, 30, 50, 70, 90],
-            labels = [];
+legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info legend'),
+        limits = [-10, 10, 30, 50, 70, 90],
+        labels = [];
         
-        for (var i = 0; i < limits.length; i++) {
-            labels.push(
-                '<div class="labels"><i style="background:' + getColor(limits[i] + 1) + '"></i>' + 
-                limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+') + '</div>');
-        }
-
-        div.innerHTML += '<ul>' + labels.join('') + '</ul>'
-        return div;
+    for (var i = 0; i < limits.length; i++) {
+        labels.push(
+            '<div class="labels"><i style="background:' + getColor(limits[i] + 1) + '"></i>' + 
+            limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+') + '</div>');
     }
 
-    legend.addTo(myMap)
-});
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div;
+}
+
+legend.addTo(myMap)
